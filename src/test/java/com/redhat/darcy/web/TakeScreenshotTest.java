68efb6b41ba9e74a107f07b7a9e31a18a552a199
Derek McNeil <dmcneil@redhat.com>
2015-10-25 13:08:12 -0400

    Update tests

--- src/test/java/com/redhat/darcy/web/TakeScreenshotTest.java
+++ src/test/java/com/redhat/darcy/web/TakeScreenshotTest.java
@@ -3,7 +3,10 @@ package com.redhat.darcy.web;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.redhat.darcy.ui.DarcyException;
 import com.redhat.darcy.web.stubs.FakeScreenshotTakingBrowser;
 
 import com.google.common.jimfs.Jimfs;
@@ -28,7 +31,35 @@ public class TakeScreenshotTest {
 
         browser.takeScreenshot(path);
 
-        assertTrue(Files.exists(path));
-        assertThat(Files.readAllBytes(path), equalTo(data));
+        assertTrue("Expected file to be created", Files.exists(path));
+        assertThat("Expected the correct data to be written to the file",
+                Files.readAllBytes(path), equalTo(data));
+    }
+
+    @Test
+    public void shouldWriteDataToPathWithParentDirectories() throws IOException {
+        byte[] data = new byte[] { 1, 2, 3 };
+        FakeScreenshotTakingBrowser browser = new FakeScreenshotTakingBrowser(data);
+
+        FileSystem fileSystem = Jimfs.newFileSystem();
+        Path path = fileSystem.getPath("parent/folder/example.png");
+
+        browser.takeScreenshot(path);
+
+        assertTrue("Expected parent directories to be created",
+                Files.exists(path.getParent()));
+        assertTrue("Expected file to be created", Files.exists(path));
+        assertThat("Expected the correct data to be written to the file",
+                Files.readAllBytes(path), equalTo(data));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(expected = DarcyException.class)
+    public void shouldThrowDarcyExceptionWhenAnIOExceptionOccurs() {
+        FakeScreenshotTakingBrowser browser = new FakeScreenshotTakingBrowser(null);
+
+        Path path = mock(Path.class);
+        when(path.getParent()).thenThrow(IOException.class);
+        browser.takeScreenshot(path);
     }
 }
